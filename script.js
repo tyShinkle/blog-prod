53ecba6d0dc380a8cf1eb4bd527325dabe8f18a6
tyShinkle <tyshinkle@gmail.com>
2023-03-30 06:10:44 -0500

    test open buttons on mobile

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-/*test top
-let el = document.getElementById("test-post");
-let testBtn = document.querySelector("#test-button");
-let testText = "test";
-let three = document.createElement("h3");
-three.textContent = testText;
-testBtn.addEventListener("click", addElement, false);
-function addElement(){
-    el.appendChild(three);
+//tyShinkle.blog
+let post = document.getElementsByClassName("post")[0];
+let postTitle = document.getElementsByClassName("post-title-card")[0];
+let postContent = document.getElementsByClassName("post-content")[0];
+let postOpenButton = document.getElementsByClassName("post-open-button")[0];
+let open = false;
+
+postOpenButton.addEventListener("click", testFunction, false);
+post.style.maxHeight = post.offsetHeight + "px";
+
+
+function testFunction(){
+    if(!open) {
+        let el = document.createElement("h3");
+        el.textContent = "HELLO!";
+        postContent.appendChild(el);
+        post.style.maxHeight = post.offsetHeight + postContent.offsetHeight + "px";
+        open = true;
+    }else {
+        post.style.maxHeight = postTitle.offsetHeight + 30 + "px";
+        open = false;
+    }
 }
-test bottom*/
+//AUTHOR: Ty Shinkle
