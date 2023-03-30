9f8c1c6847798e31174b551b14bb071816ae71c3
tyShinkle <tyshinkle@gmail.com>
2023-03-30 03:29:02 -0500

    cursor > pointer for buttons

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-/*test top*/
-
-
-/*test bottom*/
+/*test top
+let el = document.getElementById("test-post");
+let testBtn = document.querySelector("#test-button");
+let testText = "test";
+let three = document.createElement("h3");
+three.textContent = testText;
+testBtn.addEventListener("click", addElement, false);
+function addElement(){
+    el.appendChild(three);
+}
+test bottom*/
