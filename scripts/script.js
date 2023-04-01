fd918cfd1b1dc27c5c7840a12008ffccaf0fa9d1
tyShinkle <tyshinkle@gmail.com>
2023-04-01 10:55:12 -0500

    better padding

--- scripts/script.js
+++ scripts/script.js
@@ -93,7 +93,7 @@ function togglePost(e) {
         e.currentTarget.open = true;
     }
     else {
-        postParent.style.maxHeight = document.getElementById("postTitleCard" + targetIndex).offsetHeight + 100 + "px";
+        postParent.style.maxHeight = document.getElementById("postTitleCard" + targetIndex).offsetHeight + 120 + "px";
         activeButton.innerText = "+";
         e.currentTarget.open = false;
     }
