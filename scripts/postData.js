59826dce56e1e8e2d61f10f7ad56f9ed825c6e78
tyShinkle <tyshinkle@gmail.com>
2023-04-01 02:28:29 -0500

    decouple post data

---
+++ scripts/postData.js
@@ -0,0 +1,32 @@
+export const postData = `[
+    {
+        "postTitle":"Build a CRUD API with Lambda Functions and DynamoDB", 
+        "postDate":"March 31, 2023", 
+        "postTags":["#node","#AWS","#API"], 
+        "postBody":[
+            {  
+                "elementType":"heading",
+                "content":"Step One: Create a DynamoDB Table"
+            }, 
+            {
+                "elementType":"paragraph",
+                "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }
+        ]
+    },
+    {
+        "postTitle":"Git Revert Examples",
+        "postDate":"April 1, 2023",
+        "postTags":["#basics", "#git"], 
+        "postBody":[
+            {
+                "elementType":"heading",
+                "content":"Revert a Single File"
+            },
+            {
+                "elementType":"paragraph",
+                "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }
+        ]
+    }
+]`;
---
+++ scripts/script.js
@@ -0,0 +1,174 @@
+import {postData} from "./postData.js";
+
+const posts = JSON.parse(postData);
+
+let mainBody = document.getElementsByTagName("main")[0];
+
+function createPostCards() {
+    for(let i = 0; i < posts.length; i++){
+        addPost(i);
+        addTitle(i);
+        addTags(i);
+        addDate(i);
+        addButton(i);
+        setMaxHeight(i);
+    }
+}
+
+//add container div's
+function addPost(postIndex) {
+    let postDiv = document.createElement("div");
+    let postTitleDiv = document.createElement("div");
+
+    postDiv.classList.add("post");
+    postDiv.setAttribute("id", "post" + postIndex);
+
+    postTitleDiv.classList.add("post-title-card");
+    postTitleDiv.setAttribute("id", "postTitleCard" + postIndex);
+
+    postDiv.appendChild(postTitleDiv);
+    mainBody.appendChild(postDiv);
+}
+
+//add title to post
+function addTitle(postIndex) {
+    let targetPostTitleCard = document.querySelector("#postTitleCard" + postIndex); 
+    let postTitle = document.createElement("div");
+    
+    postTitle.classList.add("post-title");
+    postTitle.textContent = posts[postIndex].postTitle;
+
+    targetPostTitleCard.appendChild(postTitle);
+}
+
+//add topic tags to post
+function addTags(postIndex) {
+    let targetPostTitleCard = document.querySelector("#postTitleCard" + postIndex);
+    let postTags = document.createElement("div");
+
+    postTags.classList.add("post-tags");
+    postTags.textContent = buildTagString(postIndex);
+
+    targetPostTitleCard.appendChild(postTags);
+}
+
+//construct string out of postTags array
+function buildTagString(postIndex) {
+    let baseString = "";
+
+    for(let i=0; i < posts[postIndex].postTags.length; i++){
+        baseString += posts[postIndex].postTags[i]+ " ";
+    }
+
+    return baseString;
+}
+
+//add date to post card
+function addDate(postIndex) {
+    let targetPostTitleCard = document.querySelector("#postTitleCard" + postIndex);
+    let postDate = document.createElement("div");
+    
+    postDate.classList.add("post-date");
+    postDate.textContent = posts[postIndex].postDate;
+
+    targetPostTitleCard.appendChild(postDate);
+}
+
+//add button to each post card
+function addButton(postIndex) {
+    let targetPostTitleCard = document.querySelector("#postTitleCard" + postIndex);
+    let postButton = document.createElement("div");
+
+    postButton.classList.add("post-button");
+    postButton.setAttribute("id", "postButton" + postIndex);
+    postButton.textContent = "+";
+    postButton.addEventListener("click", openPost, false);
+    postButton.open = false;
+    postButton.fill = true;
+    postButton.postIndex = postIndex;
+
+    targetPostTitleCard.appendChild(postButton);
+}
+
+//set max height for open / close
+function setMaxHeight(postIndex) {
+    let p = document.getElementsByClassName("post")[postIndex];
+    p.style.maxHeight = p.offsetHeight + "px";
+}
+
+//open post / add content
+function openPost(e) {
+
+    let postParent = document.getElementsByClassName("post")[e.currentTarget.postIndex];
+
+    if(!e.currentTarget.open){
+        
+        if(e.currentTarget.fill){
+
+            let postContentDiv = document.createElement("div");
+            postContentDiv.classList.add("post-content");
+            postContentDiv.setAttribute("id", "postContentDiv" + e.currentTarget.postIndex);
+
+            for(let i=0; i<posts[e.currentTarget.postIndex].postBody.length; i++){
+                let childToBe = buildElement(posts[e.currentTarget.postIndex].postBody[i]);
+                postContentDiv.appendChild(childToBe);
+            }
+            postParent.appendChild(postContentDiv);
+            e.currentTarget.fill = false;
+        }
+        postParent.style.maxHeight = postParent.offsetHeight + document.getElementById("postContentDiv" + e.currentTarget.postIndex).offsetHeight + "px";
+        e.currentTarget.open = true;
+    }else {
+        //!set maxheight to the title cards previous height 
+        console.log("test");
+        postParent.style.maxHeight = document.getElementById("postTitleCard" + e.currentTarget.postIndex).offsetHeight + 30 + "px";
+        e.currentTarget.open = false;
+    }
+}
+
+//build elements off of post data
+function buildElement(el) {
+    switch(el.elementType){
+        case "heading":
+            return buildHeading(el);
+        case "paragraph":
+            return buildParagraph(el);
+        default:
+            console.log("something went wrong!");
+    }
+}
+
+//build heading
+function buildHeading(el) {
+    let elToReturn = document.createElement("h3");
+    elToReturn.textContent = el.content;
+    return elToReturn;
+}
+
+//build paragraph
+function buildParagraph(el) {
+    let elToReturn = document.createElement("p");
+    elToReturn.textContent = el.content;
+    return elToReturn;
+}
+
+//start here -> load post cards
+createPostCards();
+
+/*
+
+HTML TEMPLATE
+
+    <div class="post" id="test-post">
+        <div class="post-title-card">
+            <div class="post-title">Build a CRUD API with AWS Lambda, DynamoDB and API Gateway.</div>
+            <div class="post-tags">#API #AWS #node</div>
+            <div class="post-date">March 30 2023</div>
+            <div class="post-button" id="test-button">+</div>
+        </div>
+        <div class="post-content"></div>
+    </div>
+
+    // get number of props of object for postBody
+    console.log(Object.keys(posts[0].postBody).length);
+*/
