8f788db46b372bbc730c561fd465cedd44cb35b4
tyShinkle <tyshinkle@gmail.com>
2023-04-01 06:31:46 -0500

    extra test posts and more padding for tags

--- scripts/postData.js
+++ scripts/postData.js
@@ -2,7 +2,7 @@ export const postData = `[
     {
         "postTitle":"Build a CRUD API with Lambda Functions and DynamoDB", 
         "postDate":"March 31, 2023", 
-        "postTags":["#node","#AWS","#API"], 
+        "postTags":["#node","#aws","#api"], 
         "postBody":[
             {  
                 "elementType":"heading",
@@ -26,6 +26,36 @@ export const postData = `[
             {
                 "elementType":"paragraph",
                 "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }
+        ]
+    }, 
+    {
+        "postTitle":"Building a LAMP Server at Home",
+        "postDate":"April 2, 2023",
+        "postTags":["#linux", "#apache", "#sql", "#php"], 
+        "postBody":[
+            {
+                "elementType":"heading",
+                "content":"Step One: Grab an Old Laptop / PC"
+            },
+            {
+                "elementType":"paragraph",
+                "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            }
+        ]
+    }, 
+    {
+        "postTitle":"Mastering CSS Grid",
+        "postDate":"April 4, 2023",
+        "postTags":["#css", "#grid"], 
+        "postBody":[
+            {
+                "elementType":"heading",
+                "content":"Why use CSS Grid?"
+            },
+            {
+                "elementType":"paragraph",
+                "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             }
         ]
     }
