38383ec4f903d7797b76f95286426b4f852e229d
tyShinkle <tyshinkle@gmail.com>
2023-04-05 10:15:30 -0500

    resize content on resize test

--- scripts/windowLogic.js
+++ scripts/windowLogic.js
@@ -1,8 +1,18 @@
+let posts = document.getElementsByClassName("post"); 
+let postHeaders = document.getElementsByClassName("post-title-card");
+let postBodies = document.getElementsByClassName("post-content");
 
 export let empowerWindow = function(){
     window.addEventListener("resize", resetSizes, false);
 }
 
 let resetSizes = function() {
-    alert("resize ocurred");
+    for(let i = 0; i < posts.length; i++){
+        if(posts[i].opened){
+            posts[i].style.maxHeight = postHeaders[i].offsetHeight + postBodies[i].offsetHeight + 20 + "px";
+        }
+        else{
+            posts[i].style.maxHeight = postHeaders[i].offsetHeight + 20 + "px";
+        }
+    }
 } 
