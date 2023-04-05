5f1d7a00a88b1b77f5e16e0b5857b60908887fea
tyShinkle <tyshinkle@gmail.com>
2023-04-05 01:51:59 -0500

    decoupled menu nodes

--- scripts/navLogic.js
+++ scripts/navLogic.js
@@ -1,10 +1,6 @@
 let navButton = document.querySelector("#nav-button");
 let menuContainer = document.querySelector("#menu");
-let menuContent;
-let searchBar;
-let filterBar;
-let filterHeader;
-let filterContent;
+let menuContent = document.querySelector("#menu-content");
 
 export function empowerNav() {
     navButton.addEventListener("click", toggleNav, false);
@@ -14,10 +10,6 @@ export function empowerNav() {
 
 function toggleNav() {
     if(!menuContainer.opened){
-        if(!menuContainer.filled){
-            addNavElements();
-            menuContainer.filled = true;
-        }
         menuContainer.style.maxHeight = menuContent.offsetHeight + 20 + "px";
         menuContainer.opened = true;
         navButton.textContent = "-"
@@ -29,40 +21,3 @@ function toggleNav() {
     }
 }
 
-function addNavElements() {
-    menuContent = document.createElement("div");
-    menuContent.setAttribute("id", "menu-content");
-    addSearchBar();
-    addFilterBar();
-    menuContainer.appendChild(menuContent);
-}
-
-function addSearchBar() {
-    searchBar = document.createElement("input");
-    searchBar.setAttribute("id", "search-bar");
-    searchBar.placeholder = "# Search...";
-    menuContent.appendChild(searchBar);
-}
-
-function addFilterBar() {
-    filterBar = document.createElement("div");
-    filterHeader = document.createElement("div");
-    filterContent = document.createElement("div");
-    filterBar.setAttribute("id", "filter-bar");
-    filterHeader.setAttribute("id", "filter-header");
-    filterContent.setAttribute("id", "filter-content");
-    filterHeader.textContent = "# Filter";
-    filterBar.addEventListener("click", toggleFilter, false)
-    filterBar.appendChild(filterHeader);
-    menuContent.appendChild(filterBar);
-}
-
-function toggleFilter() {
-    fillFilter();
-    //expand filter bar
-}
-
-function fillFilter() {
-    //add tags to flex container
-    console.log("test");
-}
