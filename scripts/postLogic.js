92bfd071903ad38bfbc59ed66caf029cbf2a574c
tyShinkle <tyshinkle@gmail.com>
2023-04-05 08:25:41 -0500

    seperated build step from production logic

---
+++ scripts/postLogic.js
@@ -0,0 +1,35 @@
+let postContainers = document.getElementsByClassName("post");
+let postHeaders = document.getElementsByClassName("post-title-card");
+let postContent = document.getElementsByClassName("post-content");
+
+export let empowerPosts = function(){
+    for(let i = 0; i < postContainers.length; i++) {
+        postContainers[i].addEventListener("click", toggle, false);
+    }
+}
+
+function toggle(e){
+    let targetIndex = e.currentTarget.postIndex;
+    let opened = e.currentTarget.opened;
+    if(!opened){
+        postContainers[targetIndex].style.maxHeight = postHeaders[targetIndex].offsetHeight + postContent[targetIndex].offsetHeight + "px";
+        e.currentTarget.opened = true;
+    }
+    else{
+        postContainers[targetIndex].style.maxHeight = postHeaders[targetIndex].offsetHeight + 20 + "px";
+        e.currentTarget.opened = false;
+    }
+}
+
+/*
+HTML TEMPLATE
+    <div class="post" id="test-post">
+        <div class="post-title-card">
+            <div class="post-title">Build a CRUD API with AWS Lambda, DynamoDB and API Gateway.</div>
+            <div class="post-tags">#API #AWS #node</div>
+            <div class="post-date">March 30 2023</div>
+            <div class="post-button" id="test-button">+</div>
+        </div>
+        <div class="post-content"></div>
+    </div>
+*/
--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,7 @@
-import {postData} from "./postData.js";
-import {loadPostHeaders} from "./postHeaders.js";
+import { buildPosts } from "./buildPosts.js";
 import { empowerNav } from "./navLogic.js";
+import { empowerPosts } from "./postLogic.js";
 
-const posts = JSON.parse(postData);
-
-function entryPoint() {
-    for(let i = 0; i < posts.length; i++){
-        loadPostHeaders(i, posts[i]);
-    }
-    empowerNav();
-}
-
-entryPoint();
-
-/*
-HTML TEMPLATE
-    <div class="post" id="test-post">
-        <div class="post-title-card">
-            <div class="post-title">Build a CRUD API with AWS Lambda, DynamoDB and API Gateway.</div>
-            <div class="post-tags">#API #AWS #node</div>
-            <div class="post-date">March 30 2023</div>
-            <div class="post-button" id="test-button">+</div>
-        </div>
-        <div class="post-content"></div>
-    </div>
-*/
+buildPosts();
+empowerNav();
+empowerPosts();
