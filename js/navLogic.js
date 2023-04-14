a28cef87b0d46132b2a34a416a1c31aa2a4888b9
tyShinkle <tyshinkle@gmail.com>
2023-04-14 07:47:51 -0500

    nav border and button

--- js/navLogic.js
+++ js/navLogic.js
@@ -1,10 +1,24 @@
 let navTitle = document.querySelector("#navTitle");
+let navButton = document.querySelector("#navButton");
 
 export let empowerNav = function() {
     navTitle.addEventListener("click", goHome, false);
+    navButton.addEventListener("click", toggleNav, false);
+    navButton.pressed = false;
 }
 
 function goHome() {
     navTitle.classList.add("depressed");
     window.location.href = "http://www.tyshinkle.blog";
+}
+
+function toggleNav() {
+    if(!navButton.pressed) {
+        navButton.classList.add("depressed");
+        navButton.pressed = true;
+    }
+    else {
+        navButton.classList.remove("depressed");
+        navButton.pressed = false;
+    }
 }
