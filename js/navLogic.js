863e0d8d8006d6712e312e5c641ac5cc6de993dd
tyShinkle <tyshinkle@gmail.com>
2023-04-14 14:56:15 -0500

    search bar

--- js/navLogic.js
+++ js/navLogic.js
@@ -1,10 +1,18 @@
+let nav = document.getElementsByTagName("nav")[0];
+let navHeader = document.querySelector("#navHeader");
 let navTitle = document.querySelector("#navTitle");
 let navButton = document.querySelector("#navButton");
-let navText = document.querySelector("#navButtonText");
+let navButtonText = document.querySelector("#navButtonText");
+let navMenu = document.querySelector("#navMenu");
+let searchBar = document.querySelector("#searchBar");
+
 
 export let empowerNav = function() {
+    nav.style.maxHeight = navHeader.offsetHeight + "px";
     navTitle.addEventListener("click", goHome, false);
     navButton.addEventListener("click", toggleNav, false);
+    searchBar.addEventListener("focusin", activeSearch, false);
+    searchBar.addEventListener("focusout", inactiveSearch, false );
     navButton.pressed = false;
 }
 
@@ -18,10 +26,20 @@ function toggleNav() {
         navButton.classList.add("depressed");
         navButtonText.textContent = "-";
         navButton.pressed = true;
+        nav.style.maxHeight = navHeader.offsetHeight + navMenu.offsetHeight + "px";
     }
     else {
         navButton.classList.remove("depressed");
         navButtonText.textContent = "+";
         navButton.pressed = false;
+        nav.style.maxHeight = navHeader.offsetHeight + "px";
     }
+}
+
+function activeSearch(){
+    searchBar.classList.add("depressed");
+}
+
+function inactiveSearch(){
+    searchBar.classList.remove("depressed");
 }
